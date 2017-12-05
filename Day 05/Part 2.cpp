f5302dc893a6150c153cb9d4f1ad5ee102d8ff44
Werseter <werseter@interia.pl>
2017-12-05 23:15:42 +0100

    Added Day 05 files

---
+++ Day 05/Part 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main(void) {
+    std::ifstream input ("input.txt");
+    if(input.is_open()) {
+        std::vector<int> numbers;
+        int i, x;
+        while(input >> x) 
+            numbers.push_back(x);
+        for(x = i = 0; i < numbers.size(); (numbers[i] < 3 ? (++numbers[i], i += numbers[i] - 1) : (--numbers[i], i += numbers[i] + 1)), ++x);
+        std::cout << "Number of required jumps is " << x << " jumps" <<  std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "Failed to open input.txt file" << std::endl;
+        return 1;
+    }
+} 
+
